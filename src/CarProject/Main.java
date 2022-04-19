88c3be0a02de194efedf2c2a968d2f4812c520c9
Antonio Perez <antonio.fuster.87@gmail.com>
2022-04-19 22:28:39 +0200

    first commit

---
+++ src/CarProject/Main.java
@@ -0,0 +1,12 @@
+package CarProject;
+
+public class Main {
+    public static void main(String[] args) {
+        Car Porsche=new Car();
+        Car Holden=new Car();
+        Porsche.setModel("Carrera");
+        System.out.println("Model is " + Porsche.getModel());
+
+
+    }
+}
